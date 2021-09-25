00b3f9f2fc0db316705405c09f43d1ae7c2a3263
Joaquim Breno <62159887+JoaquimBreno@users.noreply.github.com>
2021-09-24 21:14:51 -0300

    Project

---
+++ Projeto.py
@@ -0,0 +1,26 @@
+import pandas
+
+# ----------------------------------------------------------------------------------------------
+# 
+#
+#
+#
+#
+#
+#
+#
+# ----------------------------------------------------------------------------------------------
+usuarios = pandas.read_csv("usuarios.csv", header=None)
+print("Usuarios: ")
+print(usuarios.T)
+print()
+
+
+
+conexoes = pandas.read_csv("conexoes.csv", header=None )
+print("conexoes: ")
+print(conexoes.T)
+print()
+print("conexoes T [0]")
+print(conexoes.T[0])
+
