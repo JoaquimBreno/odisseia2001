a053283e3a5580af8e1bcffd0c0fb0492d4a9142
fabiobarreto-data-science <romaobarreto@hotmail.com>
2021-09-29 21:49:24 -0300

    Update Projeto_auxiliar.py

--- Projeto_auxiliar.py
+++ Projeto_auxiliar.py
@@ -25,6 +25,9 @@ class Grafo():
          """
 
     def iniciaGrafo(self):
+        """
+         Lê os 2 arquivos, monta o grafo, organizar os dados dos usuários e ordena o grafo.
+        """
         df = pandas.read_csv("usuarios.csv", header=None)
         df = df.T
         for i in df:
@@ -47,8 +50,11 @@ class Grafo():
             self.userData[chegada]["seguidores"] += 1
 
         self._ordenaGrafo()
-        
+
     def _ordenaGrafo(self):
+        """
+        Primeiro organizamos o dicionário geral e depois ordenamos os dicionários internos, priorizando conexões peso 2
+        """
         self.userData = dict(sorted(self.userData.items()))
         self.matrizAdj = dict(sorted(self.matrizAdj.items()))
         for usuario in self.matrizAdj:
@@ -56,17 +62,29 @@ class Grafo():
             self.matrizAdj[usuario] = dict(sorted(self.matrizAdj[usuario].items(), reverse=True, key=lambda x: x[1]))
 
     def exibeSeguidos(self, usuario):
+        """
+        Acessa self.userData e imprime quantas pessoas o usuário segue.
+        """
         print(f"Usuario {usuario} segue {(self.userData[usuario]['seguindo'])} pessoas. \n")
 
     def exibeSeguidores(self, usuario):
+        """
+        Acessa self.userData e imprime quantas pessoas segue o usuário.
+        """
         print(f"Usuario {usuario} é seguido(a) por {(self.userData[usuario]['seguidores'])} pessoas. \n")
 
     def exibirStories(self, usuario):
-        print("Visualizar story de: ") 
+        """
+        Imprime username de cada usuário.
+        """
+        print(f"Visualizar story de: {usuario}")
         for key in self.matrizAdj[usuario]:
             print(key)
 
     def _topK(self, k):
+        """
+        Reordena o userData por ordem de seguidores.
+        """
         mais_seguidos = dict(sorted(self.userData.items(), reverse=True, key=lambda x: x[1]["seguidores"]))
         top = []
         for pessoa in mais_seguidos.keys():
@@ -76,13 +94,40 @@ class Grafo():
         return top[:k]
 
     def topK(self, k):
+        """
+        Imprime o '_topk'
+        """
         i = 1
         for user in self._topK(k):
             print(i, "-", user, "possui ", self.userData[user]["seguidores"], "seguidores")
             i += 1
 
     def buscaGrafo(self, usuario1, usuario2):
-        pass
+        ordemBuscaLargura = [usuario1]
+        visitados = []
+        predecessor = {usuario1: None}
+
+        while len(ordemBuscaLargura) > 0:
+            primeiro_elemento = ordemBuscaLargura[0]
+            ordemBuscaLargura = ordemBuscaLargura[1:]
+            visitados.append(primeiro_elemento)
+            for adjacente in self.matrizAdj[primeiro_elemento].keys():
+                if adjacente == usuario2:
+                    pred = primeiro_elemento
+                    caminho_invertido = [usuario2]
+                    while pred is not None:
+                        caminho_invertido.append(pred)
+                        pred = predecessor[pred]
+                    caminho = ''
+                    for no in caminho_invertido[::-1]:
+                        caminho += f'{no} -> '
+                    return print(f"O caminho entre os dois usuários informados é :{caminho[:-3]}")
+
+                if adjacente not in ordemBuscaLargura and adjacente not in visitados:
+                    predecessor[adjacente] = primeiro_elemento
+                    ordemBuscaLargura.append(adjacente)
+
+        return False
 
 
 odisseia = Grafo()
@@ -91,9 +136,10 @@ print("exibir topK: ")
 odisseia.topK(5)
 print()
 print("exibir seguidores:")
-odisseia.exibeSeguidores("helena42")
+odisseia.exibeSeguidores("sophia31")
 print()
 print("exibir seguidos:")
-odisseia.exibeSeguidos("helena42")
+odisseia.exibeSeguidos("sophia31")
 print()
-odisseia.exibirStories("helena42")
+odisseia.exibirStories("sophia31")
+odisseia.buscaGrafo("helena42", "arthur46")
